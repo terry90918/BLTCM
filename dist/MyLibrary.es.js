b345cadea1c2c8f83e37e4975d20880daa1c4620
terry90918 <zxtw17985321@gmail.com>
2023-08-18 15:46:53 +0800

    update

--- dist/MyLibrary.es.js
+++ dist/MyLibrary.es.js
@@ -1,34 +1,67 @@
-import { ref as l, openBlock as d, createElementBlock as r, toDisplayString as p, pushScopeId as i, popScopeId as u, createElementVNode as _ } from "vue";
-const a = (e, o) => {
-  const t = e.__vccOpts || e;
-  for (const [n, s] of o)
-    t[n] = s;
-  return t;
-}, v = {
+import { ref as v, openBlock as s, createElementBlock as _, toDisplayString as a, pushScopeId as h, popScopeId as m, createElementVNode as c, Fragment as i, withDirectives as f, vModelText as b, renderList as g } from "vue";
+const d = (e, t) => {
+  const o = e.__vccOpts || e;
+  for (const [l, n] of t)
+    o[l] = n;
+  return o;
+}, x = {
   __name: "HelloWorld",
   setup(e) {
-    const o = l(0), t = () => {
-      o.value++;
+    const t = v(0), o = () => {
+      t.value++;
     };
-    return (n, s) => (d(), r("button", {
+    return (l, n) => (s(), _("button", {
       type: "button",
-      onClick: t
-    }, "count is " + p(o.value), 1));
+      onClick: o
+    }, "count is " + a(t.value), 1));
   }
-}, k = /* @__PURE__ */ a(v, [["__scopeId", "data-v-8afefce5"]]);
-const f = {}, c = (e) => (i("data-v-8187ec54"), e = e(), u(), e), h = {
+}, T = /* @__PURE__ */ d(x, [["__scopeId", "data-v-4a27bb39"]]);
+const y = {}, p = (e) => (h("data-v-cfd4b654"), e = e(), m(), e), w = {
   name: "",
   id: ""
-}, m = /* @__PURE__ */ c(() => /* @__PURE__ */ _("option", { value: "" }, "1", -1)), y = /* @__PURE__ */ c(() => /* @__PURE__ */ _("option", { value: "" }, "2", -1)), x = /* @__PURE__ */ c(() => /* @__PURE__ */ _("option", { value: "" }, "3", -1)), B = [
-  m,
-  y,
-  x
+}, D = /* @__PURE__ */ p(() => /* @__PURE__ */ c("option", { value: "" }, "1", -1)), B = /* @__PURE__ */ p(() => /* @__PURE__ */ c("option", { value: "" }, "2", -1)), I = /* @__PURE__ */ p(() => /* @__PURE__ */ c("option", { value: "" }, "3", -1)), L = [
+  D,
+  B,
+  I
 ];
-function I(e, o) {
-  return d(), r("select", h, B);
+function S(e, t) {
+  return s(), _("select", w, L);
 }
-const w = /* @__PURE__ */ a(f, [["render", I], ["__scopeId", "data-v-8187ec54"]]);
+const E = /* @__PURE__ */ d(y, [["render", S], ["__scopeId", "data-v-cfd4b654"]]);
+const $ = {
+  name: "Select",
+  props: {
+    "t-drop": Boolean
+  },
+  data() {
+    return {
+      list: ["apple", "cat", "moon"],
+      something: ""
+    };
+  },
+  computed: {
+    newList() {
+      let e = [];
+      return this.something ? e = this.list.filters((t) => t.indexOf(this.something) !== -1) : e = this.list, e;
+    }
+  }
+}, k = ["value"];
+function H(e, t, o, l, n, u) {
+  return s(), _(i, null, [
+    f(c("input", {
+      type: "text",
+      "onUpdate:modelValue": t[0] || (t[0] = (r) => n.something = r)
+    }, null, 512), [
+      [b, n.something]
+    ]),
+    c("select", null, [
+      (s(!0), _(i, null, g(u.newList, (r) => (s(), _("option", { value: r }, a(r), 9, k))), 256))
+    ])
+  ], 64);
+}
+const O = /* @__PURE__ */ d($, [["render", H], ["__scopeId", "data-v-bdbd70eb"]]);
 export {
-  w as BlDropdown,
-  k as HelloWord
+  E as BlDropdown,
+  T as HelloWord,
+  O as TDropdown
 };
---
+++ src/components/TDropdown.vue
@@ -0,0 +1,40 @@
+<template>
+    <input type="text" v-model="something">
+    <select>
+        <option v-for="item in newList" :value="item">{{ item }}</option>
+    </select>
+</template>
+
+<script>
+export default {
+  name: 'Select',
+  props: {
+    't-drop': Boolean,
+  },
+  data() {
+    return {
+        list: ['apple', 'cat', 'moon'],
+        something: '',
+    }
+  },
+  computed: {
+    newList () {
+        let temp = []
+        if (this.something) {
+            temp = this.list.filters((e) => {
+                return e.indexOf(this.something) !== -1
+            });
+        } else {
+            temp = this.list
+        }
+        return temp
+    }
+  },
+}
+</script>
+
+<style scoped>
+* {
+    box-sizing: border-box;
+}
+</style>
