1de9d8c021dd2823ab8f7921886e7659d786bdb9
Lina Chen <sakura0477@gmail.com>
2023-08-18 15:04:56 +0800

    add dropdown

---
+++ src/components/BLDropdown.vue
@@ -0,0 +1,14 @@
+<template>
+    <select name="" id="">
+        <option value="">1</option>
+        <option value="">2</option>
+        <option value="">3</option>
+    </select>
+</template>
+
+
+<style scoped>
+* {
+    box-sizing: border-box;
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export { default as HelloWord } from "./components/HelloWorld.vue";
+export { default as BLDropdown } from "./components/BLDropdown.vue";
